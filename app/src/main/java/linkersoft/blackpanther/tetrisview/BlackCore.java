08032d40bf8a73633ab1a695636edc1252adef37
LiNKeR <am.usa.to.be@gmail.com>
2018-07-04 23:29:41 +0100

    beta-roll

--- app/src/main/java/linkersoft/blackpanther/tetrisview/BlackCore.java
+++ app/src/main/java/linkersoft/blackpanther/tetrisview/BlackCore.java
@@ -9,6 +9,5 @@ public class BlackCore extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.blackcore);
-
     }
 }
